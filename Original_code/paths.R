eb27a9a413eac0fecfe1636d8d03ca6b8bc9139e
Wilfrid Kendall <W.S.Kendall@warwick.ac.uk>
2018-09-06 13:35:52 +0100

    Altered test in paths.R of working directory basename from 'DetectorChecker' to 'Original_code'.

--- Original_code/paths.R
+++ Original_code/paths.R
@@ -3,6 +3,9 @@
 # PATHS 
 ###### Current working directory (required to have basename 'DetectorChecker')
 location <- getwd()
-if (! identical(basename(location), "DetectorChecker")) {
-  stop(paste("Basename of ", location, " not equal to 'DetectorChecker'"))
+# if (! identical(basename(location), "DetectorChecker")) {
+#   stop(paste("Basename of ", location, " not equal to 'DetectorChecker'"))
+# }
+if (! identical(basename(location), "Original_code")) {
+  stop(paste("Basename of ", location, " not equal to 'Original_code'"))
 }
