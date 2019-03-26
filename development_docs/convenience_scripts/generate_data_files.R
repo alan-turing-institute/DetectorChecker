c13f8411fbec40f6bf6f32a8a6c6de684e441c45
Oscar Giles <ogiles@turing.ac.uk>
2019-03-26 09:47:40 +0000

    Add remaining datasets to package

--- development_docs/convenience_scripts/generate_data_files.R
+++ development_docs/convenience_scripts/generate_data_files.R
@@ -12,21 +12,21 @@ create_dataset <- function(detector_type, file_path){
 
 }
 
-#PerkinElmer_Full
+# PerkinElmer_Full
 PerkinElmerFull_exp_1 <- create_dataset("PerkinElmerFull", "inst/extdata/PerkinElmer_Full/BadPixelMap.bpm/BadPixelMap_t1.bpm.xml")
 devtools::use_data(PerkinElmerFull_exp_1, detectorchecker)
 
 PerkinElmerFull_exp_2 <- create_dataset("PerkinElmerFull", "inst/extdata/PerkinElmer_Full/BadPixelMap.bpm/BadPixelMap_t2.bpm.xml")
 devtools::use_data(PerkinElmerFull_exp_2, detectorchecker)
 
-#PerkinElmer_Cropped
+# PerkinElmer_Cropped
 PerkinElmerCropped_exp_1 <- create_dataset("PerkinElmerCropped1600", "inst/extdata/PerkinElmer_Cropped/BadPixelMap.bmp/BadPixelMap_t1.bpm.xml")
 devtools::use_data(PerkinElmerCropped_exp_1, detectorchecker)
 
 PerkinElmerCropped_exp_2 <- create_dataset("PerkinElmerCropped1600", "inst/extdata/PerkinElmer_Cropped/BadPixelMap.bmp/BadPixelMap_t2.bpm.xml")
 devtools::use_data(PerkinElmerCropped_exp_2, detectorchecker)
 
-#PerkinElmer_Refurbished
+# PerkinElmer_Refurbished
 PerkinElmerRefurbished_exp_1 <- create_dataset("PerkinElmerRefurbished", "inst/extdata/PerkinElmer_Refurbished/BadPixelMap_0.bpm/BadPixelMap_t1.bpm.xml")
 devtools::use_data(PerkinElmerRefurbished_exp_1, detectorchecker)
 
@@ -34,3 +34,20 @@ PerkinElmerRefurbished_exp_2 <- create_dataset("PerkinElmerRefurbished", "inst/e
 devtools::use_data(PerkinElmerRefurbished_exp_2, detectorchecker)
 
 
+# Excaliber
+Excalibur_exp_1 <- create_dataset("Excalibur", 
+                                  c("inst/extdata/Excalibur/pixelmask.fem1.hdf", 
+                                    "inst/extdata/Excalibur/pixelmask.fem2.hdf",
+                                    "inst/extdata/Excalibur/pixelmask.fem3.hdf",
+                                    "inst/extdata/Excalibur/pixelmask.fem4.hdf",
+                                    "inst/extdata/Excalibur/pixelmask.fem5.hdf",
+                                    "inst/extdata/Excalibur/pixelmask.fem6.hdf"))
+                                  
+devtools::use_data(Excalibur_exp_1, detectorchecker)
+
+
+# Pilatus
+
+Pilatus_exp_1 <- create_dataset("Pilatus", "inst/extdata/Pilatus/badpixel_mask.tif")
+
+devtools::use_data(Pilatus_exp_1, detectorchecker)
