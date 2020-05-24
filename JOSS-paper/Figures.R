575ab29708fda386e99dceac5c50326dfeff3ab9
Wilfrid Kendall <W.S.Kendall@warwick.ac.uk>
2020-05-24 16:14:52 +0100

    Minor tweaks.

--- JOSS-paper/Figures.R
+++ JOSS-paper/Figures.R
@@ -7,27 +7,27 @@ pilatus_detector <- create_detector("Pilatus")
 file_path <- "../inst/extdata/Pilatus/badpixel_mask.tif"
 pilatus_detector <- load_pix_matrix(detector = pilatus_detector, file_path = file_path)
 
-pdf("image/fig2-a.pdf")
+png("image/fig2-a.png")
 plot_pixels(detector = pilatus_detector, caption = FALSE)
 dev.off()
 
-pdf("image/fig2-b.pdf")
+png("image/fig2-b.png")
 # In the next line, is adjust correct?
 plot_pixels_density(detector = pilatus_detector, adjust = 5, caption = FALSE)
 dev.off()
 
-pdf("image/fig3-a.pdf")
+png("image/fig3-a.png")
 plot_pixels_kfg(detector = pilatus_detector, func = "F", caption = FALSE)
 dev.off()
 
-pdf("image/fig3-b.pdf")
+png("image/fig3-b.png")
 plot_pixels_kfg(detector = pilatus_detector, func = "G", caption = FALSE)
 dev.off()
 
-pdf("image/fig4-a.pdf")
+png("image/fig4-a.png")
 plot_pixels_kfg(detector = pilatus_detector, func = "K", caption = FALSE)
 dev.off()
 
-pdf("image/fig4-b.pdf")
+png("image/fig4-b.png")
 plot_pixels_kfg(detector = pilatus_detector, func = "Kinhom", caption = FALSE)
 dev.off()
